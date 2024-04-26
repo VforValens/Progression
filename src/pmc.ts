7c3a3a2ed5ae83a986987ada1133c54e0919a3f5
VforValens <8889280+VforValens@users.noreply.github.com>
2024-04-26 15:52:04 -0400

    Update ammo arrays for flea up to 9x21mm.

--- src/pmc.ts
+++ src/pmc.ts
@@ -508,96 +508,89 @@ export class PMCs
         const ammo127x55LL2 = ["5cadf6ddae9215051e1c23b2", "5cadf6e5ae921500113bb973"];
         const ammo127x55LL3 = ammo127x55LL2;
         const ammo127x55LL4 = ["5cadf6e5ae921500113bb973"];
-        const ammo127x55Flea = [""];
 
         // 86x70 Ammo Arrays - LL4 only.
         const ammo86x70LL4 = ["5fc275cf85fd526b824a571a", "5fc382a9d724d907e2077dab"];
-        const ammo86x70Flea = [];
 
         // 7.62x54R Ammo Arrays
         const ammo762x54LL1 = ["64b8f7c241772715af0f9c3d", "64b8f7b5389d7ffd620ccba2"];
-        const ammo762x54LL2 = ammo762x54LL1.concat(...["64b8f7968532cf95ee0a0dbf", "5887431f2459777e1612938f"]);
+        const ammo762x54LL2 = ["64b8f7968532cf95ee0a0dbf", "5887431f2459777e1612938f"];
         const ammo762x54LL3 = ["5e023cf8186a883be655e54f", "59e77a2386f7742ee578960a"];
         const ammo762x54LL4 = ["5e023d34e8a400319a28ed44", "560d61e84bdc2da74d8b4571", "5e023d48186a883be655e551"];
-        const ammo762x54Flea = [];
+        const ammo762x54Flea = ["59e77a2386f7742ee578960a", "5e023cf8186a883be655e54f"];
 
         // 7.62x51mm Ammo Arrays
         const ammo762x51LL1 = ["5e023e88277cce2b522ff2b1"];
-        const ammo762x51LL2 = ammo762x51LL1.concat(...["5e023e6e34d52a55c3304f71"]);
+        const ammo762x51LL2 = ["5e023e6e34d52a55c3304f71"];
         const ammo762x51LL3 = ["5e023e53d4353e3302577c4c", "58dd3ad986f77403051cba8f"];
         const ammo762x51LL4 = ["58dd3ad986f77403051cba8f", "5a608bf24f39f98ffc77720e", "5a6086ea4f39f99cd479502f", "5efb0c1bd79ff02a1f5e68d9"];
-        const ammo762x51Flea = [];
+        const ammo762x51Flea = ["5e023e53d4353e3302577c4c"];
 
         // 7.62x39mm Ammo Arrays
         const ammo762x39LL1 = ["59e4d3d286f774176a36250a", "64b7af734b75259c590fa895", "64b7af5a8532cf95ee0a0dbd"];
         const ammo762x39LL2 = ["64b7af5a8532cf95ee0a0dbd"];
         const ammo762x39LL3 = ["59e4d24686f7741776641ac7", "59e4cf5286f7741778269d8a", "5656d7c34bdc2d9d198b4587"];
         const ammo762x39LL4 = ["64b7af434b75259c590fa893", "59e0d99486f7744a32234762", "601aa3d2b2bcb34913271e6d"];
-        const ammo762x39Flea = [];
+        const ammo762x39Flea = ["64b7af434b75259c590fa893", "5656d7c34bdc2d9d198b4587", "59e4cf5286f7741778269d8a", "59e4d24686f7741776641ac7"];
 
         // 7.62x35mm aka .300BLKOUT Ammo Arrays
         const ammo762x35LL1 = ["6196365d58ef8c428c287da1"];
-        const ammo762x35LL2 = ammo762x35LL1.concat(["6196364158ef8c428c287d9f"]);
+        const ammo762x35LL2 = ["6196364158ef8c428c287d9f"];
         const ammo762x35LL3 = ["5fbe3ffdf8b6a877a729ea82", "619636be6db0f2477964e710"];
         const ammo762x35LL4 = ["64b8725c4b75259c590fa899", "5fd20ff893a8961fc660a954"];
-        const ammo762x35Flea = [];
+        const ammo762x35Flea = ["64b8725c4b75259c590fa899", "619636be6db0f2477964e710", "5fbe3ffdf8b6a877a729ea82"];
 
         // 7.62x25mm TT Ammo Arrays
         const ammo762x25TTLL1 = ["573602322459776445391df1", "573601b42459776410737435", "5735ff5c245977640e39ba7e", "5735fdcd2459776445391d61"]
         const ammo762x25TTLL2 = ["5736026a245977644601dc61", "573603c924597764442bd9cb", "573603562459776430731618"];
-        const ammo762x25Flea = [];
 
         // 6.8x51 Ammo Arrays.
         const ammo68x51LL3 = ["6529302b8c26af6326029fb7"];
         const ammo68x51LL4 = ["6529243824cbe3c74a05e5c1"];
-        const ammo68x51Flea = [];
+        const ammo68x51Flea = ["6529302b8c26af6326029fb7"];
 
         // 366 TKM Ammo Arrays.
         const ammo366TKMLL1 = ["59e6658b86f77411d949b250", "59e6542b86f77411dc52a77a"];
         const ammo366TKMLL2 = ["59e655cb86f77411dc52a77b", "5f0596629e22f464da6bbdd9"];
         const ammo366TKMLL3 = ["5f0596629e22f464da6bbdd9"];
-        const ammo366TKMFlea = [];
 
         // 5.56 Ammo Arrays
         const ammo556x45LL1 = ["59e6927d86f77411da468256", "59e6918f86f7746c9f75e849"];
-        const ammo556x45LL2 = ["5c0d5ae286f7741e46554302", "59e68f6f86f7746c9f75e846", "60194943740c5d77f6705eea"];
+        const ammo556x45LL2 = ["5c0d5ae286f7741e46554302", "59e68f6f86f7746c9f75e846", "54527a984bdc2d4e668b4567", "60194943740c5d77f6705eea"];
         const ammo556x45LL3 = ["54527ac44bdc2d36668b4567"];
         const ammo556x45LL4 = ["54527ac44bdc2d36668b4567", "59e690b686f7746c9f75e848", "601949593ae8f707c4608daa"];
-        const ammo556x45Flea = [];
        
         // 5.45 Ammo Arrays
         const ammo545x39LL1 = ["56dff216d2720bbd668b4568", "56dff338d2720bbd668b4569", "56dff421d2720b5f5a8b4567", "56dff4ecd2720b5f5a8b4568"];
         const ammo545x39LL2 = ["56dff4a2d2720bbd668b456a", "56dff0bed2720bb0668b4567", "56dff2ced2720bb4668b4567"];
         const ammo545x39LL3 = ["56dff3afd2720bba668b4567", "56dff2ced2720bb4668b4567", "56dff061d2720bb5668b4567"];
         const ammo545x39LL4 = ["56dff061d2720bb5668b4567", "61962b617c6c7b169525f168", "56dfef82d2720bbd668b4567", "56dff026d2720bb8668b4567", "5c0d5e4486f77478390952fe"];
-        const ammo545x39Flea = [];
+        const ammo545x39Flea = ["56dff3afd2720bba668b4567"];
 
         // 5.7mm Ammo Arrays
         const ammo57x28LL2 = ["5cc86840d7f00c002412c56c", "5cc80f8fe4a949033b0224a2"];
         const ammo57x28LL3 = ["5cc86832d7f00c000d3a6e6c", "5cc80f67e4a949035e43bbba", "5cc80f53e4a949000e1ea4f8"];
         const ammo57x28LL4 = ["5cc80f38e4a949001152b560"];
-        const ammo57x28Flea = [];
+        const ammo57x28Flea = ["5cc80f53e4a949000e1ea4f8"];
 
-        // 4.6mm Ammo Arrays
+        // 4.6mm Ammo Arrays - MP7A2 on flea
         const ammo46x30LL3 = ["64b6979341772715af0f9c39"];
         const ammo46x30LL4 = ["5ba2678ad4351e44f824b344", "5ba26835d4351e0035628ff5"];
-        const ammo46x30Flea = [];
+        const ammo46x30Flea = ["5ba2678ad4351e44f824b344", "64b6979341772715af0f9c39"];
 
         // 9x18mm Ammo Arrays
         const ammo9x18LL1 = ["57371f8d24597761006c6a81", "573720e02459776143012541"];
         const ammo9x18LL2 = ["57372140245977611f70ee91", "573719df2459775a626ccbc2"];
-        const ammo9x18Flea = [];
 
         // 9x19mm Ammo Arrays
         const ammo9x19LL1 = ["58864a4f2459770fcc257101", "5c3df7d588a4501f290594e5"];
         const ammo9x19LL2 = ["64b7bbb74b75259c590fa897", "56d59d3ad2720bdb418b4577"]
         const ammo9x19LL3 = ["5c0d56a986f774449d5de529", "5c925fa22e221601da359b7b", "5efb0da7a29a85116f6ea05f"];
-        const ammo9x19Flea = [];
 
-        // 9x21mm Ammo Arrays
+        // 9x21mm Ammo Arrays - SR2M on flea
         const ammo9x21LL3 = ["6576f93989f0062e741ba952"];
         const ammo9x21LL4 = ["5a26ac0ec4a28200741e1e18"];
-        const ammo9x21Flea = [];
+        const ammo9x21Flea = ["6576f93989f0062e741ba952"];
 
         // 9x39mm Ammo Arrays
         const ammo9x39LL3 = ["61962d879bb3d20b0946d385"];
